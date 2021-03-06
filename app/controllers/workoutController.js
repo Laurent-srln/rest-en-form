37bf70035df6be58642fe7787c748aca524b156f
Laurent Séraline <l.seraline@gmail.com>
2021-03-06 18:46:20 +0100

    newWorkout - date must not be later than today

--- app/controllers/workoutController.js
+++ app/controllers/workoutController.js
@@ -1,6 +1,19 @@
 const { config } = require('process');
 const workoutMapper = require('../mappers/workoutMapper');
 
+const dayjs = require('dayjs');
+// Pour les timezones
+const utc = require('dayjs/plugin/utc'); // dependent on utc plugin
+const timezone = require('dayjs/plugin/timezone');
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isSameOrBefore);
+// On défini les locales
+require('dayjs/locale/fr');
+dayjs.locale('fr');
+
+
 const workoutController = {
     addWorkout: async (req, res) => {
 
@@ -14,6 +27,13 @@ const workoutController = {
             return;
 
         }
+
+        if (!dayjs(newWorkout.date).isSameOrBefore(dayjs(), 'day')) {
+
+
+            res.status(400).json({"message": `La date de l'entrainement doit ne peut être ultérieure à aujourd'hui`})
+            return;
+        };
 
         const workout = await workoutMapper.addWorkout(newWorkout, memberId);
 
--- dayjs.js
+++ dayjs.js
@@ -1,6 +1,23 @@
 const dayjs = require('dayjs');
-dayjs().format();
+// Pour les timezones
+const utc = require('dayjs/plugin/utc'); // dependent on utc plugin
+const timezone = require('dayjs/plugin/timezone');
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore');
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(isSameOrBefore);
+// On défini les locales
+require('dayjs/locale/fr');
+dayjs.locale('fr');
 
-console.log(dayjs("2021-04-01 11:00:00"));
+
+
+    // {
+    //     name: "Paris",
+    //     tz: "Europe/Paris"
+    // }
+
+console.log(dayjs('2021-02-06').tz("Europe/Paris").format('YYYY-MM-DD'));
+
+
+console.log(dayjs('2021-03-03').isSameOrBefore(dayjs(), 'day'));
