81d8b841e5926ec91f63b282f204083d44c1af51
Laurent-srln <l.seraline@gmail.com>
2022-10-11 11:53:38 +0200

    Swagger doc V2 with yaml file

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 require('dotenv').config();
-
 const express = require('express');
-
-
 const cors = require('cors');
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
+
+
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger-doc.yaml');
 
 const port = process.env.PORT || 5478;
 
 const router = require('./app/router/router');
 
-const options = {
-   definition: {
-      openapi: "3.0.0",
-      info: {
-         title: "REST'Enforme API",
-         version: "1.0.0",
-         description: "A REST API for REST'Enforme"
-      },
-      servers: [
-         // {
-         //    url: "https://app-osport.herokuapp.com/api-v1"
-         // }
-         // ,
-         {
-            url: "http://localhost:3004/api-v1"
-         }
-      ]
-   },
-   apis: [ './app/router/swagger.js']
-};
-
-const specs = swaggerJsDoc(options);
 
 const app = express();
 
-app.use("/api-v1/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 
